89fbfa343fdddc10507c69e493f55296c2bc41ae
Marcus Vinicius Gomes <marcusvinid3v@gmail.com>
2023-07-22 22:32:15 -0300

    feat: add employee fetching by id

--- app/src/main/java/rh/app/controllers/FuncionarioController.java
+++ app/src/main/java/rh/app/controllers/FuncionarioController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,14 @@ public class FuncionarioController {
     @GetMapping
     public List<Funcionario> listar() {
         return funcionarioRepository.findAll();
+    }
+
+    @GetMapping("/{funcionarioId}")
+    public ResponseEntity<Funcionario> buscar(@PathVariable Long funcionarioId) {
+        return funcionarioRepository
+            .findById(funcionarioId)
+            .map(funcionario -> ResponseEntity.ok(funcionario))
+            .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
