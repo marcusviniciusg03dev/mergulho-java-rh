eaa80c911cb3ce41422cb8f8c545fafdf330e97d
Marcus Vinicius Gomes <marcusvinid3v@gmail.com>
2023-07-22 22:35:43 -0300

    feat: add employee update by id

--- app/src/main/java/rh/app/controllers/FuncionarioController.java
+++ app/src/main/java/rh/app/controllers/FuncionarioController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,6 +42,17 @@ public class FuncionarioController {
     @ResponseStatus(HttpStatus.CREATED)
     public Funcionario cadastrar(@RequestBody Funcionario funcionario) {
         return funcionarioRepository.save(funcionario);
+    }
+
+    @PutMapping("/{funcionarioId}")
+    public ResponseEntity<Funcionario> atualizar(@PathVariable Long funcionarioId, @RequestBody Funcionario funcionario) {
+        if (!funcionarioRepository.existsById(funcionarioId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        funcionario.setId(funcionarioId);
+
+        return ResponseEntity.ok(funcionarioRepository.save(funcionario));
     }
     
 }
